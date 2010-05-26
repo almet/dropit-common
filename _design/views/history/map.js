0ba4b525e3c466a8abc1ae8eb9577c6f1a1623e2
ametaireau <ametaireau@gmail.com>
2010-05-26 17:02:46 +0200

    edit history function to return a rev_number as a second item for key, in order to order the items by rev_number

--- _design/views/history/map.js
+++ _design/views/history/map.js
@@ -5,9 +5,9 @@
 function(doc) {
     if (doc.doc_type == "http://dropit.notmyidea.org/note"){
             if (doc.root_note)
-                emit([doc.root_note, doc._id], doc);
+                emit([doc.root_note, doc.rev_number], doc);
             else
-                emit([doc._id, doc._id], doc);
+                emit([doc._id, doc.rev_number], doc);
     }
 }
 
